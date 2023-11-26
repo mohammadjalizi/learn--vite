9a9d1d942e4781dc0af73bc0d9207012173f2ebb
mjalizi1376@gmail.com <mjalizi1376@gmail.com>
2023-11-26 13:43:55 +0330

    Add Role

--- src/assets/From.jsx
+++ src/assets/From.jsx
@@ -4,6 +4,8 @@ import { useState } from "react"
 const From = () => {
 
 const[Email,Setemail]=useState("")
+const[pasword,SetPasword]=useState("")
+const[Role,SetRole]=useState("")
   return (
     <div>
         
@@ -15,11 +17,25 @@ Setemail(event.target.value)
 }} />
 
 <button onClick={()=>{
-console.log(Email)
+console.log(Email,pasword)
 
 }}>login</button>  
+<input type="pasword" placeholder="pasword" value={pasword} onChange={()=>{
 
+SetPasword(event.target.value)
 
+} }  />
+
+<select value={Role} onChange={(event)=>{
+
+SetRole(event.target.value)
+
+}}>
+
+    <option value="admin"> admin </option>
+    
+    <option value="user"> user </option>
+</select>
     </div>
   )
 }
