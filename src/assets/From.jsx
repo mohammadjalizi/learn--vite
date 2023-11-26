e06d269c8bb6381c123c28e0f6eb6425b66c1406
mjalizi1376@gmail.com <mjalizi1376@gmail.com>
2023-11-26 14:25:46 +0330

    Add contact App

--- src/assets/From.jsx
+++ src/assets/From.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const From = () => {
+    const [contact,Setcontact]=useState({
+
+        name:"",
+        LastName:"",
+        Email:"",
+        phone:"",
+    })
   return (
     <div>
 
-<div className=' text-blue-500 '> contact App  </div>
+<div className=' text-blue-500 text-center  my-10 '> contact App  </div>
+
+<div>
 
 
+<input type='text'  placeholder='Name'/>
+
+<input type='text'  placeholder='LastName' value={contact.name}/>
+<input type='text'  placeholder='Email' value={contact.Email}/>
+<input type='text'  placeholder='Phone' value={contact.phone}/>
+<button>Addcontact</button>
+</div>
     </div>
   )
 }
